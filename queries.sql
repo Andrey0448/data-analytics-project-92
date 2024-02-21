1dfb96e62ba05d7bccc6ab9e04e366125d29326b
Andrey0448 <158298798+Andrey0448@users.noreply.github.com>
2024-02-21 21:50:14 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,28 +1,21 @@
 /*считает общее количество покупателей из таблицы customers*/
-SELECT count(customer_id) AS customers_count 
+SELECT 
+   count(customer_id) AS customers_count 
 FROM customers
 
 
 /*Запрос считает количество сделок и выручку каждого продавца,выберает 10 продавцов, у которых выручка наибольшая, и сортирует данные по убыванию выручки*/
-with incom as
-   (select 
-      sal.sales_person_id
-     ,sal.product_id
-     ,count(sal.sales_id) as operations
-     ,sum(sal.quantity)*pr.price as incom
+select 
+    emp.first_name||' '|| emp.last_name as name
+   ,count(sal.sales_id) as operations
+   ,round(sum(sal.quantity*pr.price)) as incom
    from sales as sal
      left join products pr
         on sal.product_id=pr.product_id
-   group by sal.sales_person_id,sal.product_id,pr.price)
-   
-    select  emp.first_name||' '|| emp.last_name as name
-           ,sum(sal.operations) as operations
-           ,round(sum(sal.incom)) as incom
-    from incom as sal
-      inner join employees emp
-         on sal.sales_person_id=emp.employee_id
-    group by first_name,last_name
-    order by incom desc
+     left join employees emp   
+        on sal.sales_person_id=emp.employee_id
+   group by first_name,last_name
+   order by incom desc
     limit 10
 
 
@@ -48,5 +41,19 @@ with average_income_vseh  as
       on sal.sales_person_id=emp.employee_id
     CROSS JOIN average_income_vseh 
     group by first_name,last_name,average_income_vseh
-    having avg(sal.quantity*pr.price) < average_income_vseh
+    having avg(sal.quantity*pr.price) < average_income_vseh 
     order by average_income
+
+
+   /*выручка каждого продавца по дням недели*/
+select
+   emp.first_name||' '||emp.last_name as name
+   ,to_char(sal.sale_date, 'day') as weekday
+   ,round(sum(sal.quantity*pr.price)) as incom
+  from sales sal
+    left join products pr
+      on sal.product_id=pr.product_id
+    left join employees emp 
+      on sal.sales_person_id=emp.employee_id
+  group by emp.first_name,emp.last_name,sal.sale_date
+  order by to_char(sal.sale_date, 'ID'),name
