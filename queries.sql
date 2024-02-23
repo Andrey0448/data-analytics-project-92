bef72e35a485e56afa66949bb5833c7d87ade045
Andrey0448 <158298798+Andrey0448@users.noreply.github.com>
2024-02-23 16:27:58 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -33,7 +33,7 @@ with average_income_vseh  as
 )
  select
    emp.first_name||' '||emp.last_name as name
-   ,round(avg(sal.quantity*pr.price)) as average_income  
+   ,round(avg(sal.quantity*pr.price)) as average_income  --средняя выручка каждого продавца
   from sales sal
     left join products pr
       on sal.product_id=pr.product_id
@@ -46,14 +46,26 @@ with average_income_vseh  as
 
 
    /*выручка каждого продавца по дням недели*/
+with group_weekday as 
+(
 select
    emp.first_name||' '||emp.last_name as name
-   ,to_char(sal.sale_date, 'day') as weekday
-   ,round(sum(sal.quantity*pr.price)) as incom
+   ,to_char(sal.sale_date, 'day') as weekday   --приводим дату в формат названия дня недели
+   ,to_char(sal.sale_date, 'id') as number_weekday  --приводим дату в формат порядкового номера дня недели
+   ,sum(sal.quantity*pr.price) as incom
   from sales sal
     left join products pr
       on sal.product_id=pr.product_id
     left join employees emp 
       on sal.sales_person_id=emp.employee_id
-  group by emp.first_name,emp.last_name,sal.sale_date
+  group by emp.first_name,emp.last_name,sale_date
   order by to_char(sal.sale_date, 'ID'),name
+  )
+  select 
+     name
+     ,weekday
+     ,round(sum(incom)) as incom 
+   from group_weekday
+   group by name,weekday,numer_weekday
+   order by number_weekday,name
+
